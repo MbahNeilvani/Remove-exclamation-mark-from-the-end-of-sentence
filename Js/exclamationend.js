b4c58fb0827788fbe7b764866e23e5b7928d3107
Mbah Neilvani <neilvanimbah@gmail.com>
2024-04-24 19:55:13 +0100

    added code

--- Js/exclamationend.js
+++ Js/exclamationend.js
@@ -3,4 +3,12 @@
 function remove(s){
     return s.replace(/!+$/, '');
   }
-  
+
+  function remove(s){
+    let res = s;
+     for(let i = s.length-1; i >= 0; i-- ){
+       if(s[i] == "!"){res = res.slice(0, -1)}
+       else{break;}
+     }
+     return res;  
+   }
